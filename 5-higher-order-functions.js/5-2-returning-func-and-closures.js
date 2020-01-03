83b0a1aeb8f5d53752444ca028acbffd9e53591e
Traian <niculescu.traian.ct@gmail.com>
2020-01-03 21:01:04 +0200

    updated 5.2

--- 5-higher-order-functions.js/5-2-returning-func-and-closures.js
+++ 5-higher-order-functions.js/5-2-returning-func-and-closures.js
@@ -21,3 +21,19 @@ teacherQuestion('Mark');
 let designerQuestion = interviewQuestion('designer');
 designerQuestion('John');
 interviewQuestion()('Marius');
+
+function retirement(retirementAge) {
+  let msg = ' years left untile retirement';
+  return function(yearOfBirth) {
+    let age = new Date().getFullYear() - yearOfBirth;
+    console.log(`${retirementAge - age} ${msg}`);
+  };
+}
+
+let retirementUS = retirement(66);
+retirementUS(1990);
+let retirementGermany = retirement(65);
+let retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementIceland(1990);
